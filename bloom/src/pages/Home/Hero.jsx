65f8ef2fb6fec6986fc79fdc8c62d52d433b79ff
ajblocker <ajblocke@gmail.com>
2019-11-14 16:19:06 -0500

    changes

---
+++ bloom/src/pages/Home/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import { 
+//     FaAngleDown,
+// } from 'react-icons/fa'
+
+const Hero = () =>{
+    return(
+        <div id="hero">
+            <div className="img-container">
+                <div className="img-container-inner">
+                    <h1 className="title">BLOOM</h1>
+                    <h2 className="subhead"> Meet, Connect, and Learn about Life</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
